c48a6f3406855f5a9fb0e63af9cb9d4dff95f3f6
George Carpinisan <gcarpinisan.04@gmail.com>
2026-09-22 00:07:30 +0000

    Add support for cli arguments
    Usage: ./vmm [args] FILE
    
    args:
        -m string    Assign memory to the VM (K, M, G). Default: 64KiB.
        -h           Print this menu and exit.
    
    FILE: path to the kernel image (ELF format)
    
    Example: ./vmm -m 128K ./kernel
    
    Signed-off-by: George Carpinisan <gcarpinisan.04@gmail.com>

--- src/vmm.c
+++ src/vmm.c
@@ -1,16 +1,83 @@
 #include "kvm.h"
 #include "vcpu.h"
+#include "assert.h"
 
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
 
 extern const uint8_t code[];
 extern const int code_size;
 
-int main(void)
+void print_usage(char *program_name)
+{
+    printf("Usage: %s [args] FILE\n", program_name);
+    printf("\nargs:\n");
+    printf("\t-m string\tAssign memory to the VM (K, M, G). Default: 64KiB.\n");
+    printf("\t-h\t\tPrint this menu and exit.\n");
+
+    printf("\nFILE: path to the kernel image (ELF format)\n");
+
+    printf("\nExample: %s -m 128K ./kernel\n", program_name);
+}
+
+static uint64_t parse_mem(const char *str)
+{
+    char *end;
+    errno = 0;
+    uint64_t val = strtoull(str, &end, 10);
+    if (errno != 0 || end == str)
+        return 0;
+
+    if (!strcasecmp(end, "K"))
+        return val << 10;
+    if (!strcasecmp(end, "M"))
+        return val << 20;
+    if (!strcasecmp(end, "G"))
+        return val << 30;
+
+    return 0;
+}
+
+int main(int argc, char **argv)
 {
     struct vm *vm;
 
-    vm = kvm_create_vm(0x1000, 0x0000);
+    /* 64KiB default */
+    uint64_t mem_sz = 64 << 10;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "m:h")) != -1) {
+        switch (opt) {
+        case 'm':
+            mem_sz = parse_mem(optarg);
+            if (!mem_sz) {
+                fprintf(stderr, "Error: Invalid memory size: %s\n", optarg);
+                print_usage(argv[0]);
+                return 1;
+            }
+            break;
+        case 'h':
+        default:
+            print_usage(argv[0]);
+            return 1;
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Error: Missing kernel image\n");
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    const char *kernel_path = argv[optind];
+
+    int kernel_fd = open(kernel_path, O_RDONLY, 0);
+    ASSERT(kernel_fd > 0, "kernel file '%s' does not exist", kernel_path);
+
+    vm = kvm_create_vm(mem_sz, 0x0000);
     vcpu_create(vm);
 
     memcpy(vm->mem_start, code, code_size);
